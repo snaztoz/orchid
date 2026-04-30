f4a4d07fd5ec26e7934f6d47ae18ed342dab01c2
snaztoz <hafidhmn@gmail.com>
2026-04-30 12:53:55 +0700

    test: setup

---
+++ compiler/parser_tests.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <iostream>
+
+void test_it_works()
+{
+    return;
+}
+
+int main()
+{
+    test_it_works();
+
+    std::cerr << "All tests passed!" << std::endl;
+
+    return 0;
+}
